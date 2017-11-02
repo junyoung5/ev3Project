6420b91c56844cea4dd50f9d545633fcfb235d76
Junyoung Oh <21300455@handong.edu>
2017-11-02 19:30:18 +0000

    ev3

--- example.cpp
+++ example.cpp
@@ -256,7 +256,7 @@ int main()
 {     
     Crain crain;
     double dis, position;
-    int turn =0, slT = 2;
+    int turn =0, slT = 1.3;
     int i = 10, max_foot = 650, max_neck = 170, max_hand = 90;
    
     
